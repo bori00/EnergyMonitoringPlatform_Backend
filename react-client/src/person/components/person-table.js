fbab25650bfba14d33a04910bb1704dc76565700
bori00 <fazakasbori@gmail.com>
2022-10-19 12:06:09 +0300

    add starter react app

---
+++ react-client/src/person/components/person-table.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Table from "../../commons/tables/table";
+
+const columns = [
+    {
+        Header: 'Name',
+        accessor: 'name',
+    },
+    {
+        Header: 'Age',
+        accessor: 'age',
+    }
+];
+
+const filters = [
+    {
+        accessor: 'name',
+    },
+    {
+        accessor: 'age',
+    }
+];
+
+function PersonTable(props) {
+    return (
+        <Table
+            data={props.tableData}
+            columns={columns}
+            search={filters}
+            pageSize={5}
+        />
+    );
+}
+
+export default PersonTable;
